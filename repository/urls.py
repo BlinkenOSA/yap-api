41b1720588a2f6b08f4ce1f345138c90e98a5ab7
JoshBone <jozsef.bone@gmail.com>
2020-01-16 16:52:50 +0100

    feat(Collections): Added table and endpoint for collections.

--- repository/urls.py
+++ repository/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from repository.views import RecordDetail, RecordList
+from repository.views import RecordDetail, RecordList, CollectionList
 
 app_name = 'repository'
 
 urlpatterns = [
     path('records/', RecordList.as_view(), name='record-list'),
     path('records/<int:pk>/', RecordDetail.as_view(), name='record-detail'),
+    path('collections/', CollectionList.as_view(), name='collection-list'),
 ]
